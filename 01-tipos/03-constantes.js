892932fd80995a01c3fa96c4297b1a8c68fae0a6
Zarlbalt <zarlbalt2@gmail.com>
2024-06-16 16:53:02 -0600

    agregue del uso de constantes de cuando se debe de usar let y en index cambie el y puse para que funcione con el nuevo archivo js

---
+++ 01-tipos/03-constantes.js
@@ -0,0 +1,6 @@
+const nombre = "Hola Mundo"; //si colocamos const evita que se cambie este valor en el futuro
+const nombre2 = "Chanchito feliz";
+//toda variable con let puede cambiar su valor en el futuro
+//la mayoria de las veces se deberia usar const
+//let se usa para dejar cambiar el valor de la variable 
+console.log(nombre, nombre2)

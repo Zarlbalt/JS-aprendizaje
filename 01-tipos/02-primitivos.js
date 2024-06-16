afa1fdefe321cbc1c9c2677b41772dec72af9f8b
Zarlbalt <zarlbalt2@gmail.com>
2024-06-16 16:45:00 -0600

    agregue los tipos de datos primitivos

---
+++ 01-tipos/02-primitivos.js
@@ -0,0 +1,23 @@
+/*Tipos de datos
+existen los primitivos que serian los"
+-Number
+-String
+-Boolean
+-Undefined
+-Null
+
+y los de referencia
+-Array
+-Object
+-Funcions
+-Clases
+*/
+
+let numero = 1; //number
+let texto = "Hola Mundo"; //string
+let verdadero = true; //boolean
+let falso = false;
+let noDefinido; //undefined
+let undef = undefined; //el undefined puede ser un valor o un tipo de dato 
+let nulo = null; //tiene que tener un valor pero debe de ser nulo
+

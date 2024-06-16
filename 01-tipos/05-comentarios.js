ef723c01d10a4d54ef5180d4d9777445c2fe1bfc
Zarlbalt <zarlbalt2@gmail.com>
2024-06-16 17:48:45 -0600

    todo hacerca de comentarios

---
+++ 01-tipos/05-comentarios.js
@@ -0,0 +1,10 @@
+//para hacer comentario hacer //
+/**
+    o para varios  utilizar como los de 
+    en esta ocacion si 
+    es para un texto mas grande o en varias 
+    lineas.
+ */
+let numero = 42;
+
+console.log(numero);

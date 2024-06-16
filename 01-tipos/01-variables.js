1945ab889018ee2946eaeb6d7732442d56e57144
Zarlbalt <zarlbalt2@gmail.com>
2024-06-16 16:31:44 -0600

    cambie el archivo de index para poder usar el archivo de js, en variables vi las palabras reservadas y el case sensitive y practicas que se hacen

---
+++ 01-tipos/01-variables.js
@@ -0,0 +1,14 @@
+
+let nombre = 'Hola Mundo'; //let palabra reservada para poder definir variables
+// las variables deben de empezar por letra o por guion bajo
+//si se trata de utilizar un numero va a salir error
+// no podermos nombrar variables con palabras reservadas como let sino saldra error
+let NombreCompleto;
+let nombreCompleto;
+let nombre_completo;
+//javascript es case sensitive
+console.log(nombre);
+
+let apellido;
+let animal; // esta es la practica es la que se realiza normalmente
+apellido = 'Schurmann';

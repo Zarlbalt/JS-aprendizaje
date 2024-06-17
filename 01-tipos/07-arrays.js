93218f44f377b78cc2d42a9e96bba603f476d240
Zarlbalt <zarlbalt2@gmail.com>
2024-06-17 13:19:28 -0600

    cambio en index para el nuevo archivo js y creacion de archivo arrays y sobre ese se vio aspectos basicos de los arrays

---
+++ 01-tipos/07-arrays.js
@@ -0,0 +1,10 @@
+let animales = ['chanchito', 'caballo']; //array literal, puede estar vacio o agregar directamente al momento de la creacion
+console.log(animales);
+console.log(animales[0]);
+animales[2] =  'dragon';
+console.log(animales);
+
+animales[10] = 'Pez';
+console.log(animales[7]);
+console.log(typeof animales);
+console.log(animales.length)

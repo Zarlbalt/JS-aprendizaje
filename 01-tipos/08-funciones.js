0a3b044dbbaffdd6b3f807f1bdecb2564d7ea520
Zarlbalt <zarlbalt2@gmail.com>
2024-06-17 13:26:32 -0600

    lo del index del archivo js y creacion de funciones en el archivo 08

---
+++ 01-tipos/08-funciones.js
@@ -0,0 +1,11 @@
+function saludar() { //palabra reservada function para hacer la funcion despues se pone el nombre de la funcion
+    console.log('Hola mundo');
+}
+
+saludar();
+
+function suma(){
+    return 2 + 2;
+}
+//let resultado = suma ();
+console.log(suma ());

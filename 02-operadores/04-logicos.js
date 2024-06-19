9200a1e564aa0d6f9e42c12789891e040af38c68
Zarlbalt <zarlbalt2@gmail.com>
2024-06-18 23:23:24 -0600

    comparadores logicos

---
+++ 02-operadores/04-logicos.js
@@ -0,0 +1,16 @@
+//AND, OR, NOT
+
+//AND &&
+
+let mayor = true;
+let suscrito = true;
+// console.log(true && true);
+// console.log(false && true);
+console.log('operador and', mayor && suscrito);
+
+//OR
+console.log('operador or', mayor || suscrito);
+
+//NOT
+console.log('operadores not', !mayor);
+let catalogoInfantil = !mayor;

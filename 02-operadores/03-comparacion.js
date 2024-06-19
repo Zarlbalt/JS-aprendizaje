ed368aa938b75ef0d66080f863f30fcb3fabc0ba
Zarlbalt <zarlbalt2@gmail.com>
2024-06-18 23:14:24 -0600

    comparaciondes y operadores de comparacion

---
+++ 02-operadores/03-comparacion.js
@@ -0,0 +1,13 @@
+let a = 10;
+console.log(a > 5);
+console.log(a < 5);
+console.log(a >= 5);
+console.log(a <= 5);
+
+//operadores de igualdad
+
+console.log(a == 10);
+console.log(a != 10);
+console.log(a == '10'); //aqui sale true porque los == pregunta si es que el valor es igual pero no pregunta por el tipo de dato;
+console.log(a === '10'); // con 3 = ahora si pregunta si el tipo de dato es el mismo
+console.log(a !== '10'); //para distinto tipo de dato y distinto tipo de valor
